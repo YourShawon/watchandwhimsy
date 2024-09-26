a093ac9e3c469ffd5800ecac907824eda2a765b6
tanzir ibne ali <tanziribneali@gmail.com>
2024-09-26 15:38:49 +0600

    remove hydration warning & layer base

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" >
       <body className={`${lato.variable} ${lato.className}`}>
         {children}
       </body>
