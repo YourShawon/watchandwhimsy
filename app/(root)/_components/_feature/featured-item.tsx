8ff84a597ffd479e93eb72e31f21fe8356c23d82
tanzir ibne ali <tanziribneali@gmail.com>
2024-10-05 23:20:55 +0600

    404, loading, featured added

--- app/(root)/_components/_feature/featured-item.tsx
+++ app/(root)/_components/_feature/featured-item.tsx
@@ -2,7 +2,7 @@
 
 const FeaturedItem = ({item}:{item:any}) => {
     return (
-        <div className="py-[25px] px-[15px] text-center border border-green-4x flex justify-center items-center flex-col gap-[15px]">
+        <div className="py-[25px] px-[15px] text-center border border-green-4x flex justify-center items-center flex-col gap-[15px] hover:translate-y-[-6px] transition ease-in-out duration-200">
             <img src={item.image} alt={item.title} />
             <h4 style={{backgroundColor: `${item.theme}`}} className="text-[13px] font-bold inline-block rounded-sm text-green-0x hover:text-green-8x pt-[9px] px-2 pb-[6px] font-spartan">{item.title}</h4>
         </div>
